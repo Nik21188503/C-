e896dc83424057608f511103cab93982a525f4b1
Nik211885 <khacninh2020@gmail.com>
2023-04-23 11:14:37 +0700

    array and little simple method arary

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
-﻿namespace learn
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace learn
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            LearnArray();
+        }
+        static void LearnArray()
+        {
+            int[] array = { 1, 2, 5, 4 };
+            int[] cpArray=new int[array.Length];
+
+            //method in arry have sum min max binarysearch sort revers ...copyto...
+            Console.WriteLine(array.Sum());
+            Console.WriteLine(Array.BinarySearch(array,1));
+            array.CopyTo(cpArray,0);
+            foreach(int i in cpArray)
+            {
+                Console.WriteLine(i);
+            }
+            Array.Sort(array);
+            Array.Reverse(array);
+            foreach(int i in array)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
